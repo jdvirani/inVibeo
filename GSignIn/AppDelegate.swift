217d0eb92ccff37af7f52ac86c64efa1990cb8be
Haresh <dignizant@bitbucket.org>
2017-03-21 17:04:06 +0530

    456

--- GSignIn/AppDelegate.swift
+++ GSignIn/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
-        print("JD123")
+        print("JD123456")
         // Override point for customization after application launch.
         return true
     }
